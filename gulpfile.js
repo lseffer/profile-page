c21de69d88dce370ce97e5fdc95fb195df81eb64
Leonard Seffer <leonard.seffer@rovio.com>
2018-09-11 17:49:44 +0300

    Added gulp and npm and sass

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+  return gulp.src('./style/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+    }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('css', function() {
+	return gulp.src('./style/css/**/*.css')
+	.pipe(gulp.dest('./css'))
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./style/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass','css']);
